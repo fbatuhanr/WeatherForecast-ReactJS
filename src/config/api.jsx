5e1f6819f81f65b3d93eb9d2905edb42477daec3
Batuhan Ozturk <fbatuhanr@gmail.com>
2023-05-23 22:11:11 +0300

    hiding api

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,8 +1,8 @@
-const apiKey = "f2174d0ca49ab1012b879612d436c67a";
+//const apiKey = "";
 export const apiURL = (city, isFahrenheit=false) => {
 
-    return "https://api.openweathermap.org/data/2.5/weather"
-        + "?appid=" + apiKey
-        + "&units=" + (isFahrenheit ? "imperial" : "metric")
-        + "&q=" + city
+    return `https://api.openweathermap.org/data/2.5/weather
+    ?appid=${process.env.REACT_APP_API_KEY}
+    &units=${(isFahrenheit ? 'imperial' : 'metric')}
+    &q=${city}`
 }
